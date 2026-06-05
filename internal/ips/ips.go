860254047f69ac0726a45c1f62fa26d63c1f817a
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2026-06-05 23:40:00 +0200

    Ips: add Equal method

--- internal/ips/ips.go
+++ internal/ips/ips.go
@@ -6,3 +6,7 @@ type Ips struct {
 	V4 net.IP
 	V6 net.IP
 }
+
+func (i *Ips) Equal(other *Ips) bool {
+	return i.V4.Equal(other.V4) && i.V6.Equal(other.V6)
+}
---
+++ internal/ips/ips_test.go
@@ -0,0 +1,27 @@
+package ips
+
+import (
+	"net"
+	"testing"
+)
+
+func TestIpsEqual(t *testing.T) {
+	a := Ips{
+		V4: net.ParseIP("123.456.789.123"),
+		V6: net.ParseIP("2001:db8::68"),
+	}
+	b := a
+
+	c := Ips{
+		V4: net.ParseIP("987.654.321.987"),
+		V6: net.ParseIP("2002:db8::68"),
+	}
+
+	if !a.Equal(&b) {
+		t.Fatal("IPs should be equal")
+	}
+
+	if a.Equal(&c) {
+		t.Fatal("IPs should not be equal")
+	}
+}
